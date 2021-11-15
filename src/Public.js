6e482b79c95580e68bd6df78e86be7d1c5fee293
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-14 20:08:36 -0600

    Adding the public component

---
+++ src/Public.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./Container";
+
+const Public = () => {
+  return (
+    <container>
+      <h1>Public Route</h1>
+    </container>
+  );
+};
+
+export default Public;
