db2e167c1459f8b0dd0961b89a87a993bbc95645
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-18 19:02:42 -0600

    adding the protected component
    following the textbook

---
+++ src/Protected.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import Auth from "@aws-amplify";
+import { Container } from "./Container";
+
+const Protected = (props) => {
+  useEffect(() => {
+    Auth.currentAuthenicatedUser().catch(() => {
+      props.history.push("/profile");
+    });
+  }, []);
+  return (
+    <Container>
+      <h1>Protected Route</h1>
+    </Container>
+  );
+};
+
+export default Protected;
