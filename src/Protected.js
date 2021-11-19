de7ff0a9c680cca3bb7438f3011e712e2c848df9
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-19 14:35:53 -0600

    updated typo on protected page
    changes on auth import

--- src/Protected.js
+++ src/Protected.js
@@ -1,18 +1,19 @@
-import React, { useEffect } from "react";
-import Auth from "aws-amplify";
-import Container from "./Container";
+import React, { useEffect } from 'react';
+import { Auth } from 'aws-amplify';
+import Container from './Container';
 
 const Protected = (props) => {
-  useEffect(() => {
-    Auth.currentAuthenicatedUser().catch(() => {
-      props.history.push("/profile");
-    });
-  }, []);
-  return (
-    <Container>
-      <h1>Protected Route</h1>
-    </Container>
-  );
-};
+    useEffect( () => {
+        Auth.currentAuthenticatedUser()
+            .catch(() => {
+                props.history.push('/profile')
+            })
+        }, [])
+    return (
+        <Container>
+                <h1>Protected Route</h1>
+        </Container>
+    );
+}
 
-export default Protected;
+export default Protected
