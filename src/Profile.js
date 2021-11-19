df50fa52498297c831bc35e835b0b10f1c538cdb
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-19 14:33:48 -0600

    updated public page to show a little more info

--- src/Profile.js
+++ src/Profile.js
@@ -1,33 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { Auth } from "aws-amplify";
+import React, { useState, useEffect } from 'react';
+import { Auth } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import Container from "./Container";
+import Container  from './Container';
 
 const Profile = () => {
-  useEffect = (() => {
-      checkUser();
-    },[]);
+    useEffect(() => {
+        checkUser()
+    }, [])
 
-  const [user, setUser] = useState({});
-  const checkUser = async () => {
-    try {
-      const data = await Auth.currentUserPoolUser();
-      const userInfo = { username: data.username, ...data.attributes };
-      console.log(userInfo);
-      setUser(userInfo);
-    } catch (err) {
-      console.err("error");
+    const [user, setUser] = useState({})
+
+    const checkUser = async() => {
+        try {
+            const data = await Auth.currentUserPoolUser()
+            const userInfo = { username: data.username, ...data.attributes}
+            setUser(userInfo)
+        } catch (err) { console.err('error: ', err)}
     }
-  };
-  return (
-    <Container>
-      <h1>Profile</h1>
-      <h2>Username: {user.username}</h2>
-      <h3>Email: {user.email}</h3>
-      <h4>Phone: {user.phone_number}</h4>
-      <AmplifySignOut />
-    </Container>
-  );
-};
-
-export default withAuthenticator(Profile);
+    return (
+        <Container>
+            <h1>Profile</h1>
+            <h2>Username: {user.username}</h2>
+            <h3>Email: {user.email}</h3>
+            <h4>Phone: {user.phone_number}</h4>
+            <AmplifySignOut />
+        </Container>
+    )
+}
+export default withAuthenticator(Profile)
--- src/Public.js
+++ src/Public.js
@@ -3,7 +3,9 @@ import Container from './Container'
 const Public =()=> {
 return (
 <Container>
-<h1>Public route</h1>
+<h1>Welcome to project 5!</h1>
+<h2>Basic Authentication!</h2>
+<p>Sign in on the profile page and then the protected page will show you your deets!</p>
 </Container>
 )
 }
