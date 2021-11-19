8f77082fd65bf8f574ca18ed0705e7c1f94cd049
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-19 08:54:37 -0600

    fixing typo with importing container

--- src/Protected.js
+++ src/Protected.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Auth from "@aws-amplify";
-import { Container } from "./Container";
+import Container from "./Container";
 
 const Protected = (props) => {
   useEffect(() => {
