979f1edbe0f3b3b4cc2dbeeadad22fb2b26c84ad
jgvaldez84 <jgvaldez@madisoncollege.edu>
2021-11-19 15:03:59 -0600

    re-wrote protected.js to test props.history.push

--- src/Protected.js
+++ src/Protected.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import { Auth } from 'aws-amplify';
-import Container from './Container';
+import React, { useEffect } from "react";
+import { Auth } from "aws-amplify";
+import Container from "./Container";
 
 const Protected = (props) => {
-    useEffect( () => {
-        Auth.currentAuthenticatedUser()
-            .catch(() => {
-                props.history.push('/profile')
-            })
-        }, [])
-    return (
-        <Container>
-                <h1>if you made it here, that means you signed in!</h1>
-        </Container>
-    );
+useEffect(() => {
+  Auth.currentAuthenticatedUser()
+  .catch(()=>{
+    props.history.push('/profile')
+  })
+}, [])
+return (
+  <Container>
+    <h1>if you made it here, that means you signed in!</h1>
+  </Container>
+);
 }
 
-export default Protected
+export default Protected;
